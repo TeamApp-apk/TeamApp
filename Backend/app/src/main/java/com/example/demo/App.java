7fbcd6928ef15c2d1b5c54401ca2a92214970c48
ABardWithAWard <ABardWithAWard@github.com>
2024-07-12 13:05:42 +0200

    Refactorign name and straying away from "demo" word.

---
+++ Backend/app/src/main/java/com/example/demo/App.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    @Autowired
+    private UserService userService; // Autowired to use UserService
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        // Use UserService to create a User and return information
+        String userInfo = userService.createAndReturnUserInfo();
+        return String.format("Hello %s! Here is a user info: %s", name, userInfo);
+    }
+}
