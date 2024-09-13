010bfc5ea9449d7ccd5a36e5234bba4992ddff03
Luki <lukasz.skop@icloud.com>
2024-09-13 23:49:14 +0200

    improved version of chat

--- Frontend/app/src/main/java/com/example/TeamApp/chat/MessageItem.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/chat/MessageItem.kt
@@ -1,81 +1,131 @@
-package com.example.TeamApp.chat
-
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalHapticFeedback
 import com.example.TeamApp.R
-import java.time.LocalDateTime
+import com.example.TeamApp.chat.Message
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.util.Date
 import java.util.Locale
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.text.style.TextAlign
 
 @Composable
-fun MessageItem(message: Message, currentUserId: String) {
+fun MessageItem(
+    message: Message,
+    currentUserId: String,
+    previousMessageTimestamp: Long?
+) {
     val isCurrentUser = message.userId == currentUserId
 
-    // Tworzymy formatter dla polskiego formatu daty
-    val polishDateFormatter = DateTimeFormatter.ofPattern("HH:mm, dd MMMM yyyy", Locale("pl", "PL"))
+    // Formatter for displaying only hour and minutes
+    val timeFormatter = DateTimeFormatter.ofPattern("HH:mm", Locale("pl", "PL"))
 
-    // Formatowanie daty
+    // Formatter for displaying full date
+    val dateFormatter = DateTimeFormatter.ofPattern("HH:mm, dd MMMM yyyy", Locale("pl", "PL"))
+
+    // Format the timestamp to show only hour and minutes for separator
+    val formattedTime = message.timestamp?.toDate()
+        ?.toInstant()
+        ?.atZone(ZoneId.systemDefault())
+        ?.toLocalDateTime()
+        ?.format(timeFormatter) ?: ""
+
+    // Format the timestamp to show full date when expanded
     val formattedDate = message.timestamp?.toDate()
         ?.toInstant()
         ?.atZone(ZoneId.systemDefault())
         ?.toLocalDateTime()
-        ?.format(polishDateFormatter) ?: ""
+        ?.format(dateFormatter) ?: ""
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = if (isCurrentUser) Arrangement.End else Arrangement.Start
-    ) {
-        Column(
-            modifier = Modifier
-                .background(
-                    if (isCurrentUser) Color(0xFF007BFF) else Color(0xFFF2F2F2),
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(8.dp)
-        ) {
+    // State to track if the message is expanded
+    var isExpanded by remember { mutableStateOf(false) }
+
+    // Haptic feedback instance
+    val hapticFeedback = LocalHapticFeedback.current
+
+    // Calculate time difference for separator
+    val showTimestampSeparator = previousMessageTimestamp?.let {
+        val currentTimestamp = message.timestamp?.toDate()?.time ?: 0
+        (currentTimestamp - it) > 300000// 10 seconds in milliseconds
+    } ?: true
+
+    Column {
+        if (showTimestampSeparator) {
+            // Display timestamp separator
             Text(
-                text = message.userId,
-                style = MaterialTheme.typography.caption,
-                fontFamily = FontFamily(Font(R.font.proximanovaregular)),
-                color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Text(
-                text = formattedDate,
+                text = formattedTime,
                 fontFamily = FontFamily(Font(R.font.proximanovaregular)),
                 style = MaterialTheme.typography.overline,
-                color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
+                color = Color.Gray,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                textAlign = TextAlign.Center
             )
+        }
 
-            Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .clickable {
+                    // Perform haptic feedback
+                    hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                    // Toggle expanded state
+                    isExpanded = !isExpanded
+                },
+            horizontalArrangement = if (isCurrentUser) Arrangement.End else Arrangement.Start
+        ) {
+            Column(
+                modifier = Modifier
+                    .background(
+                        if (isCurrentUser) Color(0xFF007BFF) else Color(0xFFF2F2F2),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .padding(8.dp)
+            ) {
+                // Display user name
+                Text(
+                    text = message.userId,
+                    style = MaterialTheme.typography.caption,
+                    fontFamily = FontFamily(Font(R.font.proximanovaregular)),
+                    color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
+                )
 
-            Text(
-                text = message.message,
-                fontFamily = FontFamily(Font(R.font.proximanovaregular)),
-                style = MaterialTheme.typography.body1,
-                color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
-            )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                // Display message content
+                Text(
+                    text = message.message,
+                    fontFamily = FontFamily(Font(R.font.proximanovaregular)),
+                    style = MaterialTheme.typography.body1,
+                    color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
+                )
+
+                // Display full date/time only if message is expanded
+                if (isExpanded) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = formattedDate,
+                        fontFamily = FontFamily(Font(R.font.proximanovaregular)),
+                        style = MaterialTheme.typography.overline,
+                        color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
+                    )
+                }
+            }
         }
     }
 }
+
+
+
--- Frontend/app/src/main/java/com/example/TeamApp/chat/chatUI.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/chat/chatUI.kt
@@ -1,5 +1,4 @@
 import android.util.Log
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -9,14 +8,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme.colors
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,6 +23,7 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -34,9 +33,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.TeamApp.R
 import com.example.TeamApp.chat.Message
-import com.example.TeamApp.chat.MessageItem
 import com.example.TeamApp.chat.sendMessage
 import com.google.firebase.firestore.FirebaseFirestore
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,8 +43,8 @@ fun ChatScreen(eventId: String, currentUserId: String) {
     val db = FirebaseFirestore.getInstance()
     val messages = remember { mutableStateListOf<Message>() }
     var messageText by remember { mutableStateOf("") }
+    val listState = rememberLazyListState()
 
-    // Pobieranie wiadomości w czasie rzeczywistym
     LaunchedEffect(eventId) {
         val messagesRef = db.collection("events").document(eventId).collection("messages")
         messagesRef.orderBy("timestamp").addSnapshotListener { snapshot, error ->
@@ -64,51 +63,73 @@ fun ChatScreen(eventId: String, currentUserId: String) {
         }
     }
 
+    LaunchedEffect(messages.size) {
+        if (messages.isNotEmpty()) {
+            listState.animateScrollToItem(messages.size - 1)
+        }
+    }
+
     Column(modifier = Modifier.fillMaxSize()) {
-        // Wyświetlanie listy wiadomości
         LazyColumn(
+            state = listState,
             modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth(),
             contentPadding = PaddingValues(8.dp)
         ) {
+            var previousMessageTimestamp: Long? = null
+
             items(messages) { message ->
-                MessageItem(message, currentUserId)
+                MessageItem(
+                    message = message,
+                    currentUserId = currentUserId,
+                    previousMessageTimestamp = previousMessageTimestamp
+                )
+                // Update previous message timestamp
+                previousMessageTimestamp = message.timestamp?.toDate()?.time
             }
         }
 
-        // Pole do wysyłania wiadomości
-        Row(modifier = Modifier.padding(8.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically // Center alignment
+        ) {
             OutlinedTextField(
                 value = messageText,
                 onValueChange = { messageText = it },
-                placeholder = { Text("Napisz wiadomość...", fontFamily = FontFamily(Font(R.font.proximanovaregular))) },
+                placeholder = { Text("Napisz wiadomość...") },
                 modifier = Modifier.weight(1f),
                 singleLine = true,
                 colors = TextFieldDefaults.outlinedTextFieldColors(
-
-                    containerColor = Color.Transparent // Kolor tła
+                    containerColor = Color.Transparent,
+                    focusedBorderColor = Color.Black
                 )
             )
-//            Button(
-//                onClick = {
-//                    sendMessage(eventId, currentUserId, messageText)
-//                    messageText = "" // Resetowanie pola tekstowego po wysłaniu wiadomości
-//                },
-//                modifier = Modifier.padding(start = 8.dp)
-//            ) {
-//                Text("Wyślij")
-//            }
-            IconButton(onClick = { sendMessage(eventId, currentUserId, messageText)
-            messageText=""}, modifier = Modifier.size(48.dp).padding(start = 8.dp)
-            ) {
-                Icon(painter = painterResource(id = R.drawable.sendmessage), contentDescription ="send message", modifier =
-                Modifier.size(48.dp))
 
+            IconButton(
+                onClick = {
+                    if (messageText.isNotBlank()) {
+                        sendMessage(eventId, currentUserId, messageText)
+                        messageText = "" // Clear text field after sending message
+                    }
+                },
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .size(48.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.sendmessage),
+                    contentDescription = "send message",
+                    modifier = Modifier.size(40.dp) // Adjust icon size
+                )
             }
         }
     }
 }
+
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewChatScreen() {
