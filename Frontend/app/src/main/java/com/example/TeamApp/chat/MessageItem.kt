bab65291e5e09eada52dd968f41d781b9becb0be
Luki <lukasz.skop@icloud.com>
2024-09-13 18:33:23 +0200

    better looking chat, needs to be connected to chat button

--- Frontend/app/src/main/java/com/example/TeamApp/chat/MessageItem.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/chat/MessageItem.kt
@@ -13,11 +13,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import com.example.TeamApp.R
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun MessageItem(message: Message, currentUserId: String) {
     val isCurrentUser = message.userId == currentUserId
+
+    // Tworzymy formatter dla polskiego formatu daty
+    val polishDateFormatter = DateTimeFormatter.ofPattern("HH:mm, dd MMMM yyyy", Locale("pl", "PL"))
+
+    // Formatowanie daty
+    val formattedDate = message.timestamp?.toDate()
+        ?.toInstant()
+        ?.atZone(ZoneId.systemDefault())
+        ?.toLocalDateTime()
+        ?.format(polishDateFormatter) ?: ""
 
     Row(
         modifier = Modifier
@@ -28,7 +47,7 @@ fun MessageItem(message: Message, currentUserId: String) {
         Column(
             modifier = Modifier
                 .background(
-                    if (isCurrentUser) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+                    if (isCurrentUser) Color(0xFF007BFF) else Color(0xFFF2F2F2),
                     shape = RoundedCornerShape(8.dp)
                 )
                 .padding(8.dp)
@@ -36,18 +55,25 @@ fun MessageItem(message: Message, currentUserId: String) {
             Text(
                 text = message.userId,
                 style = MaterialTheme.typography.caption,
+                fontFamily = FontFamily(Font(R.font.proximanovaregular)),
                 color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
             )
+
             Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = formattedDate,
+                fontFamily = FontFamily(Font(R.font.proximanovaregular)),
+                style = MaterialTheme.typography.overline,
+                color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
             Text(
                 text = message.message,
+                fontFamily = FontFamily(Font(R.font.proximanovaregular)),
                 style = MaterialTheme.typography.body1,
-                color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = message.timestamp?.toDate()?.toString() ?: "",
-                style = MaterialTheme.typography.overline,
                 color = if (isCurrentUser) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface
             )
         }
--- Frontend/app/src/main/java/com/example/TeamApp/chat/chatUI.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/chat/chatUI.kt
@@ -1,15 +1,23 @@
 import android.util.Log
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,13 +26,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.TeamApp.R
 import com.example.TeamApp.chat.Message
 import com.example.TeamApp.chat.MessageItem
 import com.example.TeamApp.chat.sendMessage
 import com.google.firebase.firestore.FirebaseFirestore
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreen(eventId: String, currentUserId: String) {
     val db = FirebaseFirestore.getInstance()
@@ -65,21 +79,32 @@ fun ChatScreen(eventId: String, currentUserId: String) {
 
         // Pole do wysyłania wiadomości
         Row(modifier = Modifier.padding(8.dp)) {
-            TextField(
+            OutlinedTextField(
                 value = messageText,
                 onValueChange = { messageText = it },
-                placeholder = { Text("Napisz wiadomość...") },
+                placeholder = { Text("Napisz wiadomość...", fontFamily = FontFamily(Font(R.font.proximanovaregular))) },
                 modifier = Modifier.weight(1f),
-                singleLine = true
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+
+                    containerColor = Color.Transparent // Kolor tła
+                )
             )
-            Button(
-                onClick = {
-                    sendMessage(eventId, currentUserId, messageText)
-                    messageText = "" // Resetowanie pola tekstowego po wysłaniu wiadomości
-                },
-                modifier = Modifier.padding(start = 8.dp)
+//            Button(
+//                onClick = {
+//                    sendMessage(eventId, currentUserId, messageText)
+//                    messageText = "" // Resetowanie pola tekstowego po wysłaniu wiadomości
+//                },
+//                modifier = Modifier.padding(start = 8.dp)
+//            ) {
+//                Text("Wyślij")
+//            }
+            IconButton(onClick = { sendMessage(eventId, currentUserId, messageText)
+            messageText=""}, modifier = Modifier.size(48.dp).padding(start = 8.dp)
             ) {
-                Text("Wyślij")
+                Icon(painter = painterResource(id = R.drawable.sendmessage), contentDescription ="send message", modifier =
+                Modifier.size(48.dp))
+
             }
         }
     }
