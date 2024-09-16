b9f9a6188724ffe3bb12fd60dfeaeda48de03096
j/sw <146929474+Swadz12@users.noreply.github.com>
2024-09-16 17:59:40 +0200

    Navbar is now animated, and highlighted in better way. Overall user experience has been improved

--- Frontend/app/src/main/java/com/example/TeamApp/excludedUI/EventNavBar.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/excludedUI/EventNavBar.kt
@@ -8,21 +8,68 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.TeamApp.R
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.*
+import com.airbnb.lottie.compose.*
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 
 @Composable
 fun BottomNavBar(navController: NavController) {
     val currentDestination = navController.currentBackStackEntryAsState().value?.destination?.route
+
+    var isPlaying by remember { mutableStateOf(false) }
+    var isPlusPlaying by remember { mutableStateOf(false) }
+    var isProfilePlaying by remember { mutableStateOf(false) }
+    var isChatPlaying by remember { mutableStateOf(false) }
+
+    val searchComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.search))
+    val searchProgress by animateLottieCompositionAsState(
+        composition = searchComposition,
+        isPlaying = isPlaying,
+        iterations = 1,
+        speed = 2.0f
+    )
+
+    val plusComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.plus))
+    val plusProgress by animateLottieCompositionAsState(
+        composition = plusComposition,
+        isPlaying = isPlusPlaying,
+        iterations = 1,
+        speed = 1.5f
+    )
+
+    val profileComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.profile))
+    val profileProgress by animateLottieCompositionAsState(
+        composition = profileComposition,
+        isPlaying = isProfilePlaying,
+        iterations = 1,
+        speed = 1.5f
+    )
+
+    val chatComposition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.chat))
+    val chatProgress by animateLottieCompositionAsState(
+        composition = chatComposition,
+        isPlaying = isChatPlaying,
+        iterations = 1,
+        speed = 1.5f
+    )
+
+    LaunchedEffect(searchProgress, plusProgress, profileProgress) {
+        if (searchProgress == 1f) isPlaying = false
+        if (plusProgress >= 0.5f) isPlusPlaying = false
+        if (profileProgress >= 0.4f) isProfilePlaying = false
+    }
+
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
@@ -31,67 +78,129 @@ fun BottomNavBar(navController: NavController) {
             .height(50.dp)
             .background(color = Color(0xFFFFFFFF))
             .navigationBarsPadding()
-            .padding(horizontal = 40.dp, vertical = 8.dp)
+            .padding(horizontal = 30.dp, vertical = 8.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(24.dp)
+                .height(40.dp)
         ) {
-            IconButton(
-                onClick = {
-                    if (!currentDestination.equals("search")) {
-                        navController.navigate("search") {
-                            popUpTo("search") { inclusive = true }
+
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            if (currentDestination != "search") {
+                                navController.navigate("search") {
+                                    popUpTo("search") { inclusive = true }
+                                }
+                                isPlaying = true
+                            }
                         }
-                    }
-                },
-                    modifier = Modifier.size(24.dp)
+                    )
+                    .background(
+                        color = if (currentDestination == "search") Color.LightGray else Color.Transparent,
+                        shape = RoundedCornerShape(13.dp)
+                    )
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.search),
-                    contentDescription = "search",
-                    tint = if (currentDestination == "search") Color(0xFF003366) else Color.Gray,
+                LottieAnimation(
+                    composition = searchComposition,
+                    progress = { searchProgress },
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            if (currentDestination != "createEvent") {
+                                navController.navigate("createEvent") {
+                                    popUpTo("createEvent") { inclusive = true }
+                                }
+                                isPlusPlaying = true
+                            }
+                        }
+                    )
+                    .background(
+                        color = if (currentDestination == "createEvent") Color.LightGray else Color.Transparent,
+                        shape = RoundedCornerShape(13.dp)
+                    )
+            ) {
+                LottieAnimation(
+                    composition = plusComposition,
+                    progress = { plusProgress },
                     modifier = Modifier.fillMaxSize()
                 )
             }
 
-            // Ikona 'pluscircle' (zamieniona miejscami z 'search')
-            IconButton(
-                onClick = {  if (!currentDestination.equals("createEvent")) {
-                    navController.navigate("createEvent"){
-                        popUpTo("createEvent"){inclusive = true}
-                    }
-
-                } },
-                modifier = Modifier.size(24.dp)
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        //x do zmiany, gdy bedzie ui czatu
+                        onClick = {
+                            if (currentDestination != "x") {
+                                navController.navigate("x") {
+                                    popUpTo("x") { inclusive = true }
+                                }
+                                isPlusPlaying = true
+                            }
+                        }
+                    )
+                    .background(
+                        color = if (currentDestination == "x") Color.LightGray else Color.Transparent,
+                        shape = RoundedCornerShape(13.dp)
+                    )
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.pluscircle),
-                    contentDescription = "circle",
-                    tint = if (currentDestination == "createEvent") Color(0xFF003366) else Color.Gray,
+                LottieAnimation(
+                    composition = chatComposition,
+                    progress = { chatProgress },
                     modifier = Modifier.fillMaxSize()
                 )
             }
 
-            IconButton(
-                onClick = {  if (!currentDestination.equals("profile")) {
-                    navController.navigate("profile"){
-                        popUpTo("profile"){inclusive = true}
-                    }
 
-                } },
-                modifier = Modifier.size(24.dp)
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            if (currentDestination != "profile") {
+                                navController.navigate("profile") {
+                                    popUpTo("profile") { inclusive = true }
+                                }
+                                isProfilePlaying = true
+                            }
+                        }
+                    )
+                    .background(
+                        color = if (currentDestination == "profile") Color.LightGray else Color.Transparent,
+                        shape = RoundedCornerShape(13.dp)
+                    )
             ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.user),
-                    contentDescription = "user",
-                    tint = if (currentDestination == "profile") Color(0xFF003366) else Color.Gray,
+                LottieAnimation(
+                    composition = profileComposition,
+                    progress = { profileProgress },
                     modifier = Modifier.fillMaxSize()
                 )
             }
+
+
         }
     }
 }
+
+
+
