983962855c3568b6c8ccc75da45ab2a6bf091393
Luki <lukasz.skop@icloud.com>
2024-09-18 19:43:24 +0200

    test pin icon

--- Frontend/app/src/main/java/com/example/TeamApp/event/DetailsScreen.kt
+++ Frontend/app/src/main/java/com/example/TeamApp/event/DetailsScreen.kt
@@ -295,7 +295,7 @@ fun TomTomMapView(context: Context, locationID: Map<String, Coordinates>, select
                                     zoom = 15.0
                                 )
                             )
-                            val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.pin_icon)
+                            val bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.gym_pin)
                             val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false) // Adjust size here
                             val markerOptions = MarkerOptions(
                                 coordinate = GeoPoint(cords.latitude, cords.longitude),
